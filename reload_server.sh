be23fc587eb88ee149c0b7b1f4c7be4a039d7331
Fred Zhao <zhaozhichen@gmail.com>
2025-08-02 16:05:48 -0400

    Update reload_server.sh

--- reload_server.sh
+++ reload_server.sh
@@ -8,18 +8,18 @@ POETRY_PRINTER_DIR="/home/pi/projects/poetry_camera"
 # Define the systemd service name
 SERVICE_NAME="poetry-printer.service"
 
-# echo "Updating Poetry Printer project from Git repository..."
+echo "Updating Poetry Printer project from Git repository..."
 
 # Navigate to the project directory
 cd "$POETRY_PRINTER_DIR" || { echo "Error: Failed to change directory to $POETRY_PRINTER_DIR. Exiting."; exit 1; }
 
 # Pull the latest changes from the main branch
 # --rebase is a good practice to avoid merge commits for simple updates
-# git pull --rebase
+git pull --rebase
 
 # Check if the git pull was successful
 if [ $? -eq 0 ]; then
-    # echo "Git pull successful. Restarting the systemd service..."
+    echo "Git pull successful. Restarting the systemd service..."
 
     # Use sudo to stop, daemon-reload, and start the service
     sudo systemctl stop "$SERVICE_NAME"
